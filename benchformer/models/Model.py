226011809b0f5d9f4adbfc59b602c1c274fce346
niksyromyatnikov <nik@serafima.ai>
2022-01-30 20:08:49 +0200

    Fixed copyright duplication and C0123.
    Signed-off-by: niksyromyatnikov <nik@serafima.ai>

--- benchformer/models/Model.py
+++ benchformer/models/Model.py
@@ -11,18 +11,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 
 import torch
 import pytorch_lightning as pl
@@ -212,7 +200,7 @@ class Model(pl.LightningModule):
             try:
                 avg_metrics.update(
                     {'avg_' + key_name: torch.stack(
-                        [x[key_name] if type(x[key_name]) is torch.Tensor
+                        [x[key_name] if isinstance(x[key_name], torch.Tensor)
                          else torch.tensor(x[key_name]) for x in outputs if x[key_name] >= 0.0]
                     ).mean()
                      })
