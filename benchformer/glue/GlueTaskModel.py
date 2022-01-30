97344e85102a44be65b013afe95fdb4c33f0e66f
niksyromyatnikov <nik@serafima.ai>
2022-01-30 22:39:52 +0200

    Glue module: added copyrights and docstrings.
    Signed-off-by: niksyromyatnikov <nik@serafima.ai>

--- benchformer/glue/GlueTaskModel.py
+++ benchformer/glue/GlueTaskModel.py
@@ -1,3 +1,17 @@
+#  Copyright (c) 2022 The Serafima.ai Team. All rights reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import torch
 import torch.nn as nn
 from dotmap import DotMap
@@ -8,6 +22,17 @@ from ..models import TransformerForLM
 
 @register_task('GluePARusModel')
 class PARusTaskModel(TransformerForLM):
+    """
+    Task model for the PARus task (Choice of Plausible Alternatives for Russian language) from RussianSuperGlue. For
+    more info: https://russiansuperglue.com/tasks/task_info/PARus
+
+    Args:
+        configs (DotMap): Contains parameters required for the initialization and usage of the task model.
+
+    Attributes:
+        proba_fn: Activation function to be applied to the output of the next sentence prediction task.
+        metrics (list): Metrics to be calculated after validation and test steps.
+        """
 
     def __init__(self, configs: DotMap):
         super().__init__(configs.model)
@@ -16,6 +41,16 @@ class PARusTaskModel(TransformerForLM):
         self.metrics = ['accuracy']
 
     def training_step(self, batch, batch_nb) -> dict:
+        """
+        This method is used to compute the training loss.
+
+        Args:
+            batch: The output of DataLoader in the form of list, tuple, or tensor.
+            batch_nb (int): Displays index of the current batch.
+
+        Returns:
+            dict: Dictionary contains calculated loss after the network`s forward pass.
+        """
         input_ids, attention_mask, token_type_ids, nsp_labels = batch[:4]
 
         outputs = self.forward(input_ids, attention_mask, token_type_ids, next_sentence_label=nsp_labels)
@@ -23,12 +58,42 @@ class PARusTaskModel(TransformerForLM):
         return {'loss': outputs.loss}
 
     def validation_step(self, batch, batch_nb) -> dict:
+        """
+        This method is used to compute the validation loss.
+
+        Args:
+            batch: The output of DataLoader in the form of list, tuple, or tensor.
+            batch_nb (int): Displays index of the current batch.
+
+        Returns:
+            dict: Dictionary contains calculated loss after the network`s forward pass.
+        """
         return self.calculate_metrics(batch)
 
     def test_step(self, batch, batch_nb) -> dict:
+        """
+        This method is used to compute the test loss.
+
+        Args:
+            batch: The output of DataLoader in the form of list, tuple, or tensor.
+            batch_nb (int): Displays index of the current batch.
+
+        Returns:
+            dict: Dictionary contains calculated loss after the network`s forward pass.
+        """
         return self.calculate_metrics(batch, stage='test')
 
     def calculate_metrics(self, batch, stage: str = 'val') -> dict:
+        """
+        This method is used to compute the val/test loss and metrics.
+
+        Args:
+            batch: The output of DataLoader in the form of list, tuple, or tensor.
+            stage (str): Specifies the current stage of processing (validation or test).
+
+        Returns:
+            dict: Dictionary contains calculated loss and metrics after the network`s forward pass.
+        """
         input_ids, attention_mask, token_type_ids, nsp_labels = batch[:4]
 
         outputs = self.forward(input_ids, attention_mask, token_type_ids, next_sentence_label=nsp_labels)
--- benchformer/glue/TaskModelBuilder.py
+++ benchformer/glue/TaskModelBuilder.py
@@ -1,9 +1,34 @@
+#  Copyright (c) 2022 The Serafima.ai Team. All rights reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from dotmap import DotMap
 
 tasks_implementations_dict = {}
 
 
 def register_task(name: str = None) -> type:
+    """
+    This decorator function is used to register (store in a dictionary) classes of task models.
+
+    Args:
+        name (str): The key of the task model class in a dictionary. Can be derived from the class name if no name is
+            specified.
+
+    Returns:
+        type: Type of the registered class.
+    """
+
     def decorate(cls: type, task_name: str = None) -> type:
         global tasks_implementations_dict
 
@@ -20,10 +45,28 @@ def register_task(name: str = None) -> type:
 
 
 class TaskModelBuilder(object):
+    """
+    This class is a factory for task models creation.
+
+    Attributes:
+        tasks_dict (dict): Dictionary with task name as a key and class as a value.
+    """
     tasks_dict = tasks_implementations_dict
 
     @classmethod
     def build(cls, configs: DotMap):
+        """
+        Creates task model object using task name specified in configs.
+
+        Args:
+            configs (DotMap): Object contains parameters required for task model usage.
+
+        Raises:
+            KeyError: No task model found for the specified task name.
+
+        Returns:
+            Model: Created object of task model.
+        """
         try:
             return cls.tasks_dict[configs.task_name](configs)
         except KeyError:
